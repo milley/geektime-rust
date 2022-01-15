f5c604c61eb2fc73f3d2f159f06aee052f3c2355
milley <milley.gm@gmail.com>
2022-01-15 17:30:31 +0800

    merge ch02 answer

--- 02_concepts/src/shape.rs
+++ 02_concepts/src/shape.rs
@@ -30,34 +30,59 @@ impl Shape {
             Shape::Tri(t) => t.area(),
         }
     }
+    fn circumference(&self) -> f64 {
+        match self {
+            Shape::Rec(r) => r.circumference(),
+            Shape::Cir(c) => c.circumference(),
+            Shape::Tri(t) => t.circumference(),
+        }
+    }
 }
 
-trait Area {
+trait ShapeCalc {
     fn area(&self) -> f64;
+    fn circumference(&self) -> f64;
 }
 
-impl Area for Rectangle {
+impl ShapeCalc for Rectangle {
     fn area(&self) -> f64 {
         self.a * self.b
     }
-}
-
-impl Area for Circle {
-    fn area(&self) -> f64 {
-        self.r * self.r * MY_PI
+    fn circumference(&self) -> f64 {
+        self.a * 2.0 + self.b * 2.0
     }
 }
 
-impl Area for Triangle {
+impl ShapeCalc for Circle {
+    fn area(&self) -> f64 {
+        self.r * self.r * MY_PI
+    }
+    fn circumference(&self) -> f64 {
+        2.0 * self.r * MY_PI
+    }
+}
+
+impl ShapeCalc for Triangle {
     fn area(&self) -> f64 {
         let (a, b, c) = (self.a, self.b, self.c);
         let p = (a + b + c) / 2.0;
         (p * (p - a) * (p - b) * (p - c)).sqrt()
+    }
+    fn circumference(&self) -> f64 {
+        self.a + self.b + self.c
     }
 }
 
 fn main() {
     let rec = Rectangle { a: 10.0, b: 5.0 };
     let shape = Shape::Rec(rec);
-    println!("area is: {}", shape.area());
+    println!("area is: {}, circumference is: {}", shape.area(), shape.circumference());
+
+    let cir = Circle { r: 5.0 };
+    let shape = Shape::Cir(cir);
+    println!("area is: {}, circumference is: {}", shape.area(), shape.circumference());
+
+    let tri = Triangle { a: 10.0, b: 5.0, c: 5.0 };
+    let shape = Shape::Tri(tri);
+    println!("area is: {}, circumference is: {}", shape.area(), shape.circumference());
 }
